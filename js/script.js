9320c04d06fc4f191bb054ebe4409b7e5d99b066
EmersonBLopes <emersonbasiliolopes@gmail.com>
2022-09-23 17:21:22 -0300

    Desenho da forca completo

--- js/script.js
+++ js/script.js
@@ -43,9 +43,9 @@ var telaForca = document.getElementById("quadro-forca");
 const svgNS = "http://www.w3.org/2000/svg";
 
 const primeiraLinha = document.createElementNS(svgNS,"line");
-primeiraLinha.setAttributeNS(null,"x1",larguraMaximaForca()-larguraMaximaForca()*0.8);
+primeiraLinha.setAttributeNS(null,"x1",larguraMaximaForca()-larguraMaximaForca()*0.95);
 primeiraLinha.setAttributeNS(null,"y1",alturaMaximaForca()-alturaMaximaForca()*0.1);
-primeiraLinha.setAttributeNS(null,"x2",larguraMaximaForca()*0.8);
+primeiraLinha.setAttributeNS(null,"x2",larguraMaximaForca()*0.95);
 primeiraLinha.setAttributeNS(null,"y2",alturaMaximaForca()-alturaMaximaForca()*0.1);
 primeiraLinha.setAttributeNS(null, "stroke-linecap","round");
 primeiraLinha.setAttributeNS(null,"style",`stroke:${mainColor};stroke-width:12;`);
@@ -54,14 +54,92 @@ telaForca.appendChild(primeiraLinha);
 
 
 const segundaLinha = document.createElementNS(svgNS,"line");
-segundaLinha.setAttributeNS(null,"x1",larguraMaximaForca()-larguraMaximaForca()*0.7);
+segundaLinha.setAttributeNS(null,"x1",larguraMaximaForca()-larguraMaximaForca()*0.8);
 segundaLinha.setAttributeNS(null,"y1",alturaMaximaForca()-alturaMaximaForca()*0.1);
-segundaLinha.setAttributeNS(null,"x2",larguraMaximaForca()-larguraMaximaForca()*0.7);
-segundaLinha.setAttributeNS(null,"y2",alturaMaximaForca()-alturaMaximaForca()*0.8);
+segundaLinha.setAttributeNS(null,"x2",larguraMaximaForca()-larguraMaximaForca()*0.8);
+segundaLinha.setAttributeNS(null,"y2",alturaMaximaForca()-alturaMaximaForca()*0.9);
 segundaLinha.setAttributeNS(null, "stroke-linecap","round");
 segundaLinha.setAttributeNS(null,"style",`stroke:${mainColor};stroke-width:12;`);
 
 telaForca.appendChild(segundaLinha);
+
+const terceiraLinha = document.createElementNS(svgNS,"line");
+terceiraLinha.setAttributeNS(null,"x1",larguraMaximaForca()-larguraMaximaForca()*0.8);
+terceiraLinha.setAttributeNS(null,"y1",alturaMaximaForca()-alturaMaximaForca()*0.9);
+terceiraLinha.setAttributeNS(null,"x2",larguraMaximaForca()-larguraMaximaForca()*0.4);
+terceiraLinha.setAttributeNS(null,"y2",alturaMaximaForca()-alturaMaximaForca()*0.9);
+terceiraLinha.setAttributeNS(null, "stroke-linecap","round");
+terceiraLinha.setAttributeNS(null,"style",`stroke:${mainColor};stroke-width:12;`);
+
+telaForca.appendChild(terceiraLinha);
+
+const quartaLinha = document.createElementNS(svgNS,"line");
+quartaLinha.setAttributeNS(null,"x1",larguraMaximaForca()-larguraMaximaForca()*0.4);
+quartaLinha.setAttributeNS(null,"y1",alturaMaximaForca()-alturaMaximaForca()*0.9);
+quartaLinha.setAttributeNS(null,"x2",larguraMaximaForca()-larguraMaximaForca()*0.4);
+quartaLinha.setAttributeNS(null,"y2",alturaMaximaForca()-alturaMaximaForca()*0.8);
+quartaLinha.setAttributeNS(null, "stroke-linecap","round");
+quartaLinha.setAttributeNS(null,"style",`stroke:${mainColor};stroke-width:12;`);
+
+telaForca.appendChild(quartaLinha);
+
+const cabeca = document.createElementNS(svgNS,"circle");
+cabeca.setAttributeNS(null,"cx",larguraMaximaForca()-larguraMaximaForca()*0.4);
+cabeca.setAttributeNS(null,"cy",alturaMaximaForca()-alturaMaximaForca()*0.7);
+cabeca.setAttributeNS(null,"r",alturaMaximaForca()-alturaMaximaForca()*0.92);
+cabeca.setAttributeNS(null,"style",`stroke:${mainColor};stroke-width:12;fill:${backgroundColor};`);
+
+telaForca.appendChild(cabeca);
+
+const tronco = document.createElementNS(svgNS,"line");
+tronco.setAttributeNS(null,"x1",larguraMaximaForca()-larguraMaximaForca()*0.4);
+tronco.setAttributeNS(null,"y1",alturaMaximaForca()-alturaMaximaForca()*0.62);
+tronco.setAttributeNS(null,"x2",larguraMaximaForca()-larguraMaximaForca()*0.4);
+tronco.setAttributeNS(null,"y2",alturaMaximaForca()-alturaMaximaForca()*0.39);
+tronco.setAttributeNS(null, "stroke-linecap","round");
+tronco.setAttributeNS(null,"style",`stroke:${mainColor};stroke-width:12;`);
+
+telaForca.appendChild(tronco);
+
+const pernaDireita = document.createElementNS(svgNS,"line");
+pernaDireita.setAttributeNS(null,"x1",larguraMaximaForca()-larguraMaximaForca()*0.4);
+pernaDireita.setAttributeNS(null,"y1",alturaMaximaForca()-alturaMaximaForca()*0.39);
+pernaDireita.setAttributeNS(null,"x2",larguraMaximaForca()-larguraMaximaForca()*0.45);
+pernaDireita.setAttributeNS(null,"y2",alturaMaximaForca()-alturaMaximaForca()*0.20);
+pernaDireita.setAttributeNS(null, "stroke-linecap","round");
+pernaDireita.setAttributeNS(null,"style",`stroke:${mainColor};stroke-width:12;`);
+
+telaForca.appendChild(pernaDireita);
+
+const pernaEsquerda = document.createElementNS(svgNS,"line");
+pernaEsquerda.setAttributeNS(null,"x1",larguraMaximaForca()-larguraMaximaForca()*0.4);
+pernaEsquerda.setAttributeNS(null,"y1",alturaMaximaForca()-alturaMaximaForca()*0.39);
+pernaEsquerda.setAttributeNS(null,"x2",larguraMaximaForca()-larguraMaximaForca()*0.35);
+pernaEsquerda.setAttributeNS(null,"y2",alturaMaximaForca()-alturaMaximaForca()*0.20);
+pernaEsquerda.setAttributeNS(null, "stroke-linecap","round");
+pernaEsquerda.setAttributeNS(null,"style",`stroke:${mainColor};stroke-width:12;`);
+
+telaForca.appendChild(pernaEsquerda);
+
+const bracoDireito = document.createElementNS(svgNS,"line");
+bracoDireito.setAttributeNS(null,"x1",larguraMaximaForca()-larguraMaximaForca()*0.4);
+bracoDireito.setAttributeNS(null,"y1",alturaMaximaForca()-alturaMaximaForca()*0.55);
+bracoDireito.setAttributeNS(null,"x2",larguraMaximaForca()-larguraMaximaForca()*0.48);
+bracoDireito.setAttributeNS(null,"y2",alturaMaximaForca()-alturaMaximaForca()*0.35);
+bracoDireito.setAttributeNS(null, "stroke-linecap","round");
+bracoDireito.setAttributeNS(null,"style",`stroke:${mainColor};stroke-width:12;`);
+
+telaForca.appendChild(bracoDireito);
+
+const bracoEsquerdo = document.createElementNS(svgNS,"line");
+bracoEsquerdo.setAttributeNS(null,"x1",larguraMaximaForca()-larguraMaximaForca()*0.4);
+bracoEsquerdo.setAttributeNS(null,"y1",alturaMaximaForca()-alturaMaximaForca()*0.55);
+bracoEsquerdo.setAttributeNS(null,"x2",larguraMaximaForca()-larguraMaximaForca()*0.32);
+bracoEsquerdo.setAttributeNS(null,"y2",alturaMaximaForca()-alturaMaximaForca()*0.35);
+bracoEsquerdo.setAttributeNS(null, "stroke-linecap","round");
+bracoEsquerdo.setAttributeNS(null,"style",`stroke:${mainColor};stroke-width:12;`);
+
+telaForca.appendChild(bracoEsquerdo);
 
 function alturaMaximaForca(){
 
