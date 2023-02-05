15a74cb98ef879dae5a03ea5a13a4081c66a7442
EmersonBLopes <emersonbasiliolopes@gmail.com>
2023-02-04 22:09:31 -0300

    Implementando funcionalidades aos botões secundarios desistir e cancelar

--- js/index.js
+++ js/index.js
@@ -1,20 +1,25 @@
 import Palavra from "./Palavra.js";
 
-  var palavra = new Palavra();
+const botaoDesistir = document.querySelector("[data-desistir]");
+var palavra = new Palavra();
 
-  //transforma o html collection em Array
-  var tecladoVirtual = Array.from(document.getElementsByClassName("tecla-virtual"));
-  
-  //adiciona um evento listerner para cada tecla do teclado virtual 
-  tecladoVirtual.forEach((tecla)=>{
-    tecla.addEventListener("click", (evento)=>{
-      var teclaClicada = evento.srcElement;
-      palavra.comparar(teclaClicada);
-    })
+//transforma o html collection em Array
+var tecladoVirtual = Array.from(document.getElementsByClassName("tecla-virtual"));
+
+//adiciona um evento listerner para cada tecla do teclado virtual 
+tecladoVirtual.forEach((tecla)=>{
+  tecla.addEventListener("click", (evento)=>{
+    var teclaClicada = evento.srcElement;
+    palavra.comparar(teclaClicada);
   })
+})
 
-  //escuta entradas do teclado
-  document.addEventListener("keydown",(evento)=>{
-    var tecla = document.querySelector(`[data-letra="${evento.key}"]`);
-    palavra.comparar(tecla);
-  });
+//escuta entradas do teclado
+document.addEventListener("keydown",(evento)=>{
+  var tecla = document.querySelector(`[data-letra="${evento.key}"]`);
+  palavra.comparar(tecla);
+});
+
+botaoDesistir.addEventListener("click",()=>{
+  location.replace("https://emersonblopes.github.io/OneJogoDaForca/index.html");
+})
--- js/input/pegaInput.js
+++ js/input/pegaInput.js
@@ -1,6 +1,7 @@
 import tratarInput from "./tratamentoInput.js";
 const input = document.querySelector("#texto");
 const botaoSalvar = document.querySelector("[data-salvar]");
+const botaoCancelar = document.querySelector("[data-cancelar]");
 
 input.addEventListener("keydown",(evento) => {
   if(evento.which === 13){
@@ -11,4 +12,9 @@ input.addEventListener("keydown",(evento) => {
 botaoSalvar.addEventListener("click",(evento)=>{
   evento.preventDefault();
   tratarInput(input);
+})
+
+botaoCancelar.addEventListener("click",(evento)=>{
+  evento.preventDefault();
+  location.replace("https://emersonblopes.github.io/OneJogoDaForca/index.html");
 })
