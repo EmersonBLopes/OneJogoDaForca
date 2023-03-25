43040a937bcea2603e3a7e415732425786e1bc55
EmersonBLopes <emersonbasiliolopes@gmail.com>
2023-03-25 15:16:28 -0300

    documentando a classe requisicao e criando um método para verificar se é possível estabelecer conexão com o servidor

--- js/API/Requisicao.js
+++ js/API/Requisicao.js
@@ -1,7 +1,13 @@
 export default class Requisicao{
 
-  #URL = "https://apipalavrasspring-production.up.railway.app/";
+  //#URL = "https://apipalavrasspring-production.up.railway.app/";
+  #URL = "http://localhost:8080/";
 
+  /**
+   *@method solicitarPalavrasAleatorias solicita um número especifico de palavras ao servidor
+   *@param {number} numeroDePalavras quantidade maxíma de palavras que será retornada pelo servidor
+   *@return {Array} array de objetos
+   */
   solicitarPalavrasAleatorias(numeroDePalavras){
 
     const httpRequest = new XMLHttpRequest();
@@ -13,6 +19,11 @@ export default class Requisicao{
     return JSON.parse(resposta);
   }
 
+  /**
+   * @method adicionarPalavra envia a palavra recebida pelo input para o servidor
+   * @param {string} palavra palavra a qual será enviada para o servidor
+   * @return {string} mensagem de retorno do servidor
+   */
   async adicionarPalavra(palavra){
     const resposta = await Promise.resolve(fetch(this.#URL+"solicitar",{ method: "POST", headers:{ "Content-Type" : "text/plain" }, body: palavra})
       .then((promise) => promise.text())
@@ -20,5 +31,22 @@ export default class Requisicao{
     );
 
     return resposta;
+  }
+
+  /**
+   * @method verificarConexao verifica a conexão com o servidor
+   * @return retorna verdadeiro se conseguir estabelecer conexão com o sevirdor senão retorna false
+   */
+  async verificarConexao(){
+  
+   let resposta = true; 
+
+    try{
+      await fetch(this.#URL+"palavras-aleatorias");
+    }catch{
+      resposta = !resposta;
+    }
+
+  return resposta;
   }
 }
--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
+import Requisicao from "./API/Requisicao.js";
+
+const solicitador = new Requisicao();
+
 const botaoPrimario = document.querySelector("[data-comecar]");
 const botaoSecundario = document.querySelector("[data-sugerir]");
 
+console.log( await solicitador.verificarConexao());
+
 botaoPrimario.addEventListener("click", ()  => window.location = "./pages/jogo_da_forca.html");
 botaoSecundario.addEventListener("click", () => window.location = "./pages/adicionar.html");
+
