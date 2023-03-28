823d4ce70d7d95670ba483371a8ce24a43a170e1
EmersonBLopes <emersonbasiliolopes@gmail.com>
2023-03-27 22:32:26 -0300

    Adicionando aviso caso servidor não responda

--- js/index.js
+++ js/index.js
@@ -7,6 +7,25 @@ const botaoSecundario = document.querySelector("[data-sugerir]");
 
 const disponibilidadeDoServidor = await solicitador.verificarConexao();
 
+if(!disponibilidadeDoServidor){
+
+  const avisoServidor = document.createElement("div");
+  const mensagemServidor = document.createElement("span");
+  const iconeAviso = document.createElement("i");
+  const secaoPrincipal = document.querySelector("[data-principal]");
+  
+  iconeAviso.classList.add("fa-solid");
+  iconeAviso.classList.add("fa-triangle-exclamation");
+
+  mensagemServidor.innerHTML = " Servidor indisponível"
+
+  avisoServidor.append(iconeAviso);
+  avisoServidor.append(mensagemServidor);
+
+  avisoServidor.classList.add("principal__mensagem-servidor");
+  secaoPrincipal.prepend(avisoServidor);
+}
+
 botaoPrimario.addEventListener("click", ()  => window.location = "./pages/jogo_da_forca.html");
 botaoSecundario.addEventListener("click", () => window.location = "./pages/adicionar.html");
 
